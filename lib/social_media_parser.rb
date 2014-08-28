703eef26ed79fd70bb35935ccadf5200efa4a397
Markus Nordin <markus.nordin@mynewsdesk.com>
2014-08-28 10:20:26 +0200

    Support string keys in passed hash

--- lib/social_media_parser.rb
+++ lib/social_media_parser.rb
@@ -2,6 +2,14 @@ require 'social_media_parser/base'
 
 module SocialMediaParser
   def self.parse(profile_attributes)
-    Base.new(profile_attributes)
+    Base.new(symbolize_keys(profile_attributes))
+  end
+
+  private
+
+  def self.symbolize_keys(hash)
+    new_hash = Hash.new
+    hash.each{|k,v| new_hash[k.to_sym] = v}
+    new_hash
   end
 end
